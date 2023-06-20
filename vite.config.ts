dc28d93925e922c39aefb791e3b44440f69f5f9a
Bidesh <bideshbanerjee444@gmail.com>
2023-06-20 19:46:03 +0530

    chat func added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Chat App",
+    short_name: "chats",
+    description: "know your nearest and farthest one with chats",
+    
+    theme_color: "grey",
+    background_color: "grey",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(),VitePWA(manifestForPlugin)],
 })
